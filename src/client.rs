3656d2ecaeaba053347b91d35a1717962f9fa0e8
Mark Lopez <m@silvenga.com>
2026-04-02 19:10:56 -0500

    chore: speculative fixes

--- src/client.rs
+++ src/client.rs
@@ -34,20 +34,10 @@ pub static HTTPS_CONNECTOR: LazyLock<HttpsConnector<HttpConnector>> = LazyLock::
 	hyper_rustls::HttpsConnectorBuilder::new()
 		.with_tls_config(
 			rustls::ClientConfig::builder()
-				// These are the Firefox 145.0 cipher suite,
-				// minus the suites missing forward-secrecy support,
-				// in the same order.
-				// https://github.com/redlib-org/redlib/issues/446#issuecomment-3609306592
 				.with_cipher_suites(&[
 					rustls::cipher_suite::TLS13_AES_256_GCM_SHA384,
 					rustls::cipher_suite::TLS13_AES_128_GCM_SHA256,
 					rustls::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				])
 				// .with_safe_default_cipher_suites()
 				.with_safe_default_kx_groups()
